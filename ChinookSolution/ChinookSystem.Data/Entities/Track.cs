6b321dee59a506fd2b9cffaff75434b2af5f1cf9
CallaSize <54920964+CallaSize@users.noreply.github.com>
2019-09-09 20:50:07 -0600

    Sep 9

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Track.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Tracks")]
+    public class Track
+    {
+        [Key]
+        public int TrackId { get; set; }
+        public string AlbumId { get; set; }
+        public int MediaTypeId{ get; set; }
+        public int GenreId { get; set; }
+
+        public string Composer { get; set; }
+
+        public int Milliseconds { get; set; }
+
+        public int Bytes { get; set; }
+
+        public int UnitPrice{ get; set; }
+
+        private string _Name;
+
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Name = null;
+                }
+                else
+                {
+                    _Name = value;
+                }
+            }
+        }
+
+
+        //navigational properties for relationships between entities
+        public virtual Genre Genre { get; set; }
+        public virtual MediaType MediaType { get; set; }
+        public virtual Album Album { get; set; }
+
+    }
+}
