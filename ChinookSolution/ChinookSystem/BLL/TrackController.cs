06d18535fd959516230485314514ffc39fa6e5a6
CallaSize <54920964+CallaSize@users.noreply.github.com>
2019-10-23 19:50:46 -0600

    TracksforPlaylistSelection
    We used some programming logic in the BLL in order to return the information our POCO TrackList contains and union the things together.

--- ChinookSolution/ChinookSystem/BLL/TrackController.cs
+++ ChinookSolution/ChinookSystem/BLL/TrackController.cs
@@ -53,11 +53,74 @@ namespace ChinookSystem.BLL
         {
             using (var context = new ChinookContext())
             {
-                List<TrackList> results = null;
 
-               //code to go here
 
-                return results;
+                //we have to consider that there is a string value and the type for the artist
+                //for album we also have a string and the type so when we do teh lookup, my where clause, value contains....whatever my string is
+                //and of a table type= x , but we can't do that with the genre or the media type, because even though it comes in as a string, I need an int and tabletype
+                //the where clause will be similar pkey=int and the table type =p and we will have another one, pkey=int and tabletype =k.
+                //probelm is that we want 1 return with 2 different reports. 
+
+                //check the incoming parameters and if needed set to a default value, ODS's fire when your page comes up
+                if (string.IsNullOrEmpty(tracksby))
+                {
+                    tracksby = ""; //mainly to catch the null situation
+                }
+                if (string.IsNullOrEmpty(arg))
+                {
+                    arg = "";
+                } //now we know we  have an object, this is not going to be empty.
+                //next, we set these values to a particular item (id/string)
+                //create 2 local variables representing the argument as a) an integer b) as a string
+                int argid = 0;
+                string argstring = "zyxzz"; //<--selected something that would likely never exist as my default value
+                //determine if incoming argument should be integer or string
+                if (tracksby.Equals("Genre") || tracksby.Equals("MediaType"))
+                {
+                    argid = int.Parse(arg);
+                }
+                else
+                {
+                    argstring = arg.Trim();
+                }
+                var results = (from x in context.Tracks
+                               where (  x.GenreId == argid && tracksby.Equals("Genre"))
+                                        || (x.MediaTypeId == argid && tracksby.Equals("MediaType"))
+                               select new TrackList
+                               {
+                                   TrackID = x.TrackId,
+                                   Name = x.Name,
+                                   Title = x.Album.Title,
+                                   ArtistName = x.Album.Artist.Name,
+                                   MediaName = x.MediaType.Name,
+                                   GenreName = x.Genre.Name,
+                                   Composer = x.Composer,
+                                   Milliseconds = x.Milliseconds,
+                                   Bytes = x.Bytes,
+                                   UnitPrice = x.UnitPrice
+                               }
+                               ).Union(
+                                        from x in context.Tracks
+                                        //going to change the style in which we ask this, so now the ternary operator is up!
+                                        where tracksby.Equals("Artist") ? x.Album.Artist.Name.Contains(argstring) : 
+                                                tracksby.Equals("Album") ? x.Album.Title.Contains(argstring) : false
+                                                  
+                                        select new TrackList
+                                        {
+                                            TrackID = x.TrackId,
+                                            Name = x.Name,
+                                            Title = x.Album.Title,
+                                            ArtistName = x.Album.Artist.Name,
+                                            MediaName = x.MediaType.Name,
+                                            GenreName = x.Genre.Name,
+                                            Composer = x.Composer,
+                                            Milliseconds = x.Milliseconds,
+                                            Bytes = x.Bytes,
+                                            UnitPrice = x.UnitPrice
+                                        }
+                                    );
+
+                return results.ToList();
             }
         }//eom
 
