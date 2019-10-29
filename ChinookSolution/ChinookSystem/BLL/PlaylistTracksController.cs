b2e1865e4e99e47d13fa3b0a722176227e510f16
CallaSize <54920964+CallaSize@users.noreply.github.com>
2019-10-28 20:38:05 -0600

    Add Track to Playlist
    limit only unqiue tracks on playlist

--- ChinookSolution/ChinookSystem/BLL/PlaylistTracksController.cs
+++ ChinookSolution/ChinookSystem/BLL/PlaylistTracksController.cs
@@ -10,6 +10,7 @@ using ChinookSystem.Data.DTOs;
 using ChinookSystem.Data.POCOs;
 using ChinookSystem.DAL;
 using System.ComponentModel;
+using DMIT2018Common.UserControls;
 #endregion
 
 namespace ChinookSystem.BLL
@@ -55,9 +56,81 @@ namespace ChinookSystem.BLL
         {
             using (var context = new ChinookContext())
             {
-                //code to go here
-                
-             
+                //use the BusinessRuleExecption to throw errors to the webpage. Local list of all the errors to throw at end.
+                List<string> reasons = new List<string>();
+                PlaylistTrack newTrack = null;
+                int tracknumber = 0;
+
+                //Part One
+                //determine if the playlist exists
+                //query the table using the playlistname and username
+                //if the playlist exists, one will get a record.
+                //if the playlist does not exist, one will get a null.
+                //to ensure these results, the query will be wrapped in a .FirstOrDefault() <-- return the first occurence of what it finds, or if it doesn't find, it returns null value.
+                //Playlist exists = context.Playlists
+                //                    .Where(x => x.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+                //                    && x.Name.Equals(playlistname, StringComparison.OrdinalIgnoreCase))
+                //                    .Select(x => x)
+                //                    .FirstOrDefault();
+                //now see in query-syntax:
+                Playlist exists = (from x in context.Playlists
+                                   where x.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+                                         && x.Name.Equals(playlistname, StringComparison.OrdinalIgnoreCase)
+                                   select x).FirstOrDefault();
+
+                //does the playlist exist?
+                if (exists==null)
+                {
+                    //this is a new playlist so i need to create the playlist record
+                    exists = new Playlist();
+                    exists.Name = playlistname;
+                    exists.UserName = username;
+                    //stage the add
+                    exists = context.Playlists.Add(exists);
+                    //since this is a new playlist the track number will be equal to 1
+                    tracknumber = 1;
+                }
+                else
+                {
+                    //since the playlist exists, so may the track exist on the playlisttracks
+                    newTrack = exists.PlaylistTracks.SingleOrDefault(x => x.TrackId == trackid);
+                    if (newTrack == null)
+                    {
+                        tracknumber = exists.PlaylistTracks.Count() + 1;
+                    }
+                    else
+                    {
+                        reasons.Add("Track already exists on the playlist");
+                    }
+
+                }
+                //Part two
+                //create the playlistTrack entry
+                //if there are any reasons not to create then, throw the businessRuleExceptio
+                if (reasons.Count() > 0)
+                {
+                    //issue with adding the track
+                    throw new BusinessRuleException("Adding track to playlist", reasons);
+                }
+                else
+                {
+                    //use the Playlist navigation to PlaylistTracks to do the add to the PlaylistTracks
+                    newTrack = new PlaylistTrack();
+                    newTrack.TrackId = trackid;
+                    newTrack.TrackNumber = tracknumber;
+                    //how do i fill the playlist id? if the playlist is brandnew?! What's a girl gonna do? A brand new playlist, does not yet have an id!
+                    //heres the secret NOTE: tthe pkey for PlaylistID may not yet exist. using the nvaigational property on the PlayList entity, one can
+                    //let HashSet handle the PlaylistId pkey value to be properly create on PlaList adnplaced correctly in the "child" record of PlaylistTracks
+                    //what is wrong is to attempt this:
+                    //newTrack.PlaylistId = exists.PlaylistId
+                    exists.PlaylistTracks.Add(newTrack); // playlist track staging
+
+                    //physically add any/all data to the database
+                    //commit
+                    context.SaveChanges();
+                }
+
+
             }
         }//eom
         public void MoveTrack(string username, string playlistname, int trackid, int tracknumber, string direction)
